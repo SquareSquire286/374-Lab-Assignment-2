5b970d73df86ecb80fc90422f699f87d7322e87c
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2019-10-03 15:30:31 -0600

    Added comments

--- ListElement.java
+++ ListElement.java
@@ -8,17 +8,17 @@ public class ListElement
 
     public ListElement()
     {
-	this.data = 0;
+	this.data = 0; // default constructor, setting null values for the pointers and 0 for the data value
 	this.next = null;
 	this.previous = null;
     }
     
-    public void setData(int i)
+    public void setData(int i) // since the 'data' attribute is private, it must be accessed with a setter and a getter
     {
 	this.data = i;
     }
 
-    public void setNext(ListElement le)
+    public void setNext(ListElement le) // since the 'next' attribute is private, it must be accessed with a setter and a getter
     {
 	this.next = le;
     }
